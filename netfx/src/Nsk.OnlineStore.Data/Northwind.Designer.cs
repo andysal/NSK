7fd093a7ec5a970fb6cbd13f821c8171d526a5af
Andrea Saltarello <andysal@gmail.com>
2018-03-27 16:56:14 +0200

    Server-side packages references updated

--- netfx/src/Nsk.OnlineStore.Data/Northwind.Designer.cs
+++ netfx/src/Nsk.OnlineStore.Data/Northwind.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Andrea\Progetti\Nsk_Afresh\src\Nsk.OnlineStore.Data\Northwind.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Dev\Github\NSK\netfx\src\Nsk.OnlineStore.Data\Northwind.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
